c1490b76f41ff423762a2465d8c9beb7416aab34
Murat Çelik <muratcelikdev@gmail.com>
2021-10-26 20:05:43 +0300

    feat: create color constants

---
+++ src/common/colors.ts
@@ -0,0 +1,14 @@
+export const base = '#1ea4ce';
+export const white = '#ffffff';
+export const lightPink = '#f2f0fd';
+
+export const black100 = '#697488';
+export const black200 = '#191919';
+
+export const grey50 = '#fafafa';
+export const grey100 = '#f3f0fe';
+export const grey200 = '#dfdee2';
+export const grey300 = '#c4c4c4';
+export const grey400 = '#e0e0e0';
+export const grey460 = '#a8a8a8';
+export const grey500 = '#525252';
