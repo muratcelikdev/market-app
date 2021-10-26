709dc40d97f368c512c25ebccbdfec1828e5b6f4
Murat Çelik <muratcelikdev@gmail.com>
2021-10-26 20:31:19 +0300

    feat: create screen interval constants

---
+++ src/styles/screen.ts
@@ -0,0 +1,15 @@
+export const breakpoints = {
+  largeScreenBreakpoint: 1200,
+};
+
+const intervals = {
+  smallScreenMaxWidth: breakpoints.largeScreenBreakpoint - 1,
+  largeScreenMinWidth: breakpoints.largeScreenBreakpoint,
+};
+
+const screens = {
+  smallScreen: `(max-width: #{${intervals.smallScreenMaxWidth}px})`,
+  largeScreen: `(max-width: #{${intervals.largeScreenMinWidth}px})`,
+};
+
+export default screens;
