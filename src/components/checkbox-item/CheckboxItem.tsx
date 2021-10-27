f2b70035eea1f8303f541d096386b170ee697390
Murat Çelik <muratcelikdev@gmail.com>
2021-10-27 11:28:46 +0300

    feat: create checkbox item component

---
+++ src/components/checkbox-item/CheckboxItem.tsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+import * as colors from 'common/colors';
+
+import Checkbox from 'components/checkbox/Checkbox';
+
+interface ICheckboxItem {
+  value: any;
+  text: string;
+  info: number | string;
+  onSelect: (value: any) => void;
+}
+
+const StyledCheckboxItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledCheckboxContent = styled(StyledCheckboxItemContainer)`
+  margin-left: 8px;
+`;
+
+const StyledCheckboxText = styled.div`
+  margin-right: 4px;
+
+  font-size: 14px;
+  line-height: 1.29;
+  letter-spacing: 0.16px;
+
+  color: ${colors.grey500};
+`;
+
+const StyledCheckboxInfo = styled(StyledCheckboxText)`
+  margin: 0;
+
+  color: ${colors.grey460};
+
+  &:before {
+    content: '(';
+  }
+
+  &:after {
+    content: ')';
+  }
+`;
+
+const CheckboxItem = ({ value, text, info, onSelect }: ICheckboxItem): JSX.Element => {
+  return (
+    <StyledCheckboxItemContainer>
+      <Checkbox value={value} onSelect={onSelect} />
+      <StyledCheckboxContent>
+        <StyledCheckboxText>{text}</StyledCheckboxText>
+        <StyledCheckboxInfo>{info}</StyledCheckboxInfo>
+      </StyledCheckboxContent>
+    </StyledCheckboxItemContainer>
+  );
+};
+
+export default CheckboxItem;
