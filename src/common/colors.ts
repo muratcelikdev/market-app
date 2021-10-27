6597546c632f232b690ce26b8a0e5c583554523e
Murat Çelik <muratcelikdev@gmail.com>
2021-10-27 10:34:09 +0300

    feat: add shadow color

--- src/common/colors.ts
+++ src/common/colors.ts
@@ -14,3 +14,5 @@ export const grey300 = '#c4c4c4';
 export const grey400 = '#e0e0e0';
 export const grey460 = '#a8a8a8';
 export const grey500 = '#525252';
+
+export const shadow = 'rgba(93, 56, 192, 0.4)';
