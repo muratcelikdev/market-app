bfd3457b4ce5ce15f1a2b1990568982c544e0d13
Murat Çelik <muratcelikdev@gmail.com>
2021-10-27 17:22:37 +0300

    feat: implement app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import GlobalStyle from 'styles/globalStyles';
+import BasketList from 'components/basket-list/BasketList';
+import Navbar from 'components/navbar/Navbar';
+
+import FilterArea from 'modules/filter-area/FilterArea';
+import ProductArea from 'modules/product-area/ProductArea';
+
+import { AppLayout, MainPageLayout } from 'styles/components';
 
 const App = () => (
   <>
-    <GlobalStyle />
+    <Navbar />
+    <AppLayout>
+      <MainPageLayout>
+        <FilterArea />
+        <ProductArea />
+        <BasketList />
+      </MainPageLayout>
+    </AppLayout>
   </>
 );
 
