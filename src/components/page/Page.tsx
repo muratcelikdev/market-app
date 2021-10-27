0ef3bb81e8fe841b2c6a78c823348ae045a147e6
Murat Çelik <muratcelikdev@gmail.com>
2021-10-27 17:16:34 +0300

    chore: change page max width value & add page interface

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 
+interface IPage {
+  children?: JSX.Element | JSX.Element[];
+  className?: string;
+}
+
 const StyledPageContainer = styled.div`
   width: 100%;
-  max-width: 1440px;
+  max-width: 1232px;
 `;
 
-const Page = ({ children }): JSX.Element => {
-  return <StyledPageContainer>{children}</StyledPageContainer>;
+const Page = ({ children, className }: IPage): JSX.Element => {
+  return <StyledPageContainer className={className}>{children}</StyledPageContainer>;
 };
 
 export default Page;
