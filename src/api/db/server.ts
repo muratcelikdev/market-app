08c300f23b823c599f778673edb33f52db9cf1e3
Murat Çelik <muratcelikdev@gmail.com>
2021-10-29 01:52:12 +0300

    chore: change json-server port

--- src/api/db/server.ts
+++ src/api/db/server.ts
@@ -8,6 +8,6 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(5000, () => {
+server.listen(4000, () => {
   console.log('JSON Server is running!');
 });
