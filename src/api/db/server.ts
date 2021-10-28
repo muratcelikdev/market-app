5543162c84dcf97d0f618ae01ff7aec16c4c68ce
Murat Çelik <muratcelikdev@gmail.com>
2021-10-28 18:14:53 +0300

    feat: install json-server and create mock data server

---
+++ src/api/db/server.ts
@@ -0,0 +1,13 @@
+// @ts-ignore
+const path = require('path');
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, './json/db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(5000, () => {
+  console.log('JSON Server is running!');
+});
