20865579069d634ffbb39aa8289b06ffa54fe864
Murat Çelik <muratcelikdev@gmail.com>
2021-10-29 13:31:52 +0300

    feat: add tagList, itemTypes and brandList in store

--- src/state/slices/itemsSlice.ts
+++ src/state/slices/itemsSlice.ts
@@ -7,13 +7,53 @@ export const getItems = createAsyncThunk('items/getItems', async () => {
   return response.data;
 });
 
+export interface ItemProps {
+  tags: Array<string>;
+  price: number;
+  name: string;
+  description: string;
+  slug: string;
+  added: number;
+  manufacturer: string;
+  itemType: string;
+}
+
+interface StateProps {
+  itemList: Array<ItemProps>;
+  tagList: Array<string>;
+  itemTypes: Array<string>;
+  brandList: Array<string>;
+}
+
+const state: StateProps = {
+  itemList: [],
+  tagList: [],
+  itemTypes: [],
+  brandList: [],
+};
+
 const itemsSlice = createSlice({
   name: 'items',
-  initialState: { itemList: [] },
+  initialState: state,
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(getItems.fulfilled, (state, action) => {
+      const tagList = Array.from(
+        new Set([].concat.apply([], Array.from(action.payload.map((item) => item.tags))))
+      ) as Array<string>;
+
+      const itemTypes = Array.from(
+        new Set(action.payload.map((item) => item.itemType))
+      ) as Array<string>;
+
+      const brandList = Array.from(
+        new Set(action.payload.map((item) => item.manufacturer))
+      ) as Array<string>;
+
       state.itemList = action.payload;
+      state.tagList = tagList;
+      state.itemTypes = itemTypes;
+      state.brandList = brandList;
     });
   },
 });
